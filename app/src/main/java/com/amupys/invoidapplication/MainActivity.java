eaf71513c6ef3787fd8733044472bfbf1ed04775
amit8899 <raiamit343@gmail.com>
2021-07-30 18:23:21 +0530

    Library

--- app/src/main/java/com/amupys/invoidapplication/MainActivity.java
+++ app/src/main/java/com/amupys/invoidapplication/MainActivity.java
@@ -3,12 +3,28 @@ package com.amupys.invoidapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.amupys.myinvoidlibrary.InvoidVerification;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.btn_verify);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InvoidVerification invoid = new InvoidVerification();
+                invoid.verify(MainActivity.this, WelcomeActivity.class);
+            }
+        });
     }
 }
